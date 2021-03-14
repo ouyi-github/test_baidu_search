40fbe876cdbc054b0c1c564126c1f9c67dde8e35
ouyi <2412632321@qq.com>
2021-03-14 18:53:55 +0800

    two_commit

--- test_baidu_search.py
+++ test_baidu_search.py
@@ -1,9 +1,11 @@
 import os
 import time
 
+
 import allure
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
@@ -17,8 +19,8 @@ class TestBaiduSearch:
         now_dir = os.getcwd()
         while True:
             now_dir = os.path.split(now_dir)
-            if now_dir[1] == 'test_baidu_search':
-                now_dir = os.path.join(now_dir[0], 'test_baidu_search')
+            if now_dir[1] == 'test_baidu_search_nogit':
+                now_dir = os.path.join(now_dir[0], 'test_baidu_search_nogit')
                 break
             now_dir = now_dir[0]
         return now_dir
@@ -27,7 +29,11 @@ class TestBaiduSearch:
         """前置动作"""
         # driver_path = os.path.join(self.get_dir(),'plugin/windows/chromedriver.exe') # Windows下使用
         driver_path = os.path.join(self.get_dir(), 'plugin/linux/chromedriver') # linux下使用
-        self.driver = webdriver.Chrome(executable_path=driver_path)
+        option = Options()
+        option.add_argument("--headless") # linux下使用无头浏览器需要添加这个参数
+        option.add_argument("--no-sandbox") # 表示不用跟用户运行chrome
+        option.add_argument("--disable-dev-shm-usage") #
+        self.driver = webdriver.Chrome(executable_path=driver_path,chrome_options=option)
         self.driver.maximize_window()
         self.driver.implicitly_wait(5)
 
@@ -37,10 +43,7 @@ class TestBaiduSearch:
 
 
     @allure.story('百度搜索测试用例')
-    @pytest.mark.parametrize("name", [("华为"), ("腾讯"), ("阿里"), ("百度"), ("字节"), ("平安"), ("python"), ("java"), ("c语言"),
-                                      ("shell"), ("docker"), ("selenium"), ("pytest"), ("django"), ("flask"), ("四川"),
-                                      ("成都"),
-                                      ("老虎"), ("猴子"), ("狮子"), ("狗"), ("猫"), ("欧毅")])
+    @pytest.mark.parametrize("name", [("狗"), ("猫"), ("欧毅")])
     def test_baidu_search(self, name):
         self.driver.get("https://www.baidu.com/")
         time.sleep(5)
